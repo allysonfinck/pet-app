0739904eec49c37de9991ed4f9c2ea025cb047f5
Natalie Wolff <natalielynnwolff@gmail.com>
2018-04-02 18:43:02 -0400

    form built, post route

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,46 @@
 const app = angular.module('PetApp', []);
 
 app.controller('MainController', ['$http', function($http){
+  const controller = this;
 
   this.appName = 'Pet Finder'
 
-}])
+
+this.getPets = function(){
+  $http({
+    method:'GET',
+    url:'/pets',
+  }).then(function(response){
+    controller.pets = response.data
+  }, function(){
+    console.log('error');
+  })
+}
+this.getPets();
+
+this.createPets = function(){
+  $http({
+    method:'POST',
+    url:'/pets',
+    data:{
+      animal: this.animal,
+      breed: this.breed,
+      size: this.size,
+      sex: this.sex,
+      location: this.location,
+      age: this.age
+    }
+  }).then(function(response){
+    controller.getPets();
+  }, function(){
+    console.log('error');
+  })
+}
+
+
+
+
+
+
+
+}]) //closes app.controller
