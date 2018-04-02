4cbda5cd5bad1bcceeeb4664254e3c2bc5e6f8e9
Allyson Finck <allyson.finck@gmail.com>
2018-04-02 18:42:30 -0400

    created users, sessions, controllers and routes

---
+++ controllers/sessions.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users.js');
+const bcrypt = require('bcrypt');
+
+router.post('/', (req, res)=>{
+  User.findOne({username: req.body.username}, (err, foundUser)=>{
+    if(bcrypt.compareSync(req.body.password, foundUser.password)){
+      req.session.currentUser = foundUser;
+      res.status(201).json({
+        status: 201,
+        message: 'Session Created'
+      })
+    } else {
+      res.status(401).json({
+        status: 401,
+        message: 'Login Failed'
+      })
+    }
+  })
+})
+
+router.delete('/', (req, res)=>{
+  req.session.destroy(()=>{
+    res.status(200).json({
+      status: 200,
+      message: 'Logout Complete'
+    })
+  })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(session({
+  secret: 'feedmeseymour',
+  resave: false,
+  saveUninitialized: false
+}))
+
+const usersController = require('./controllers/users.js');
+app.use('/users', usersController);
+
+const sessionsController = require('./controllers/sessions.js');
+app.use('/sessions', sessionsController);
+
+const petsController = require('./controllers/pets.js');
+app.use('/pets', petsController);
 
 mongoose.connect('mongodb://localhost:27017/petapp');
 mongoose.connection.once('open', ()=>{
