6db967e91403e581885ee1cf4c9f9c244cdd7ede
Allyson Finck <allyson.finck@gmail.com>
2018-04-04 12:35:49 -0400

    reduced ajax errors

--- public/js/app.js
+++ public/js/app.js
@@ -7,20 +7,19 @@ app.controller('MainController', ['$http', function($http){
 
   this.pets = [];
 
-  this.baseURL = 'http://api.petfinder.com/my.method?key=12345&arg1=foo';
-  this.apikey = 'apikey=' + 'd9f0af5e7a062cb5c0fec14bb266231b';
+  this.baseURL = 'http://api.petfinder.com/pet.findPets?key=d9f0af5e7a062cb5c0fec14bb266231b&arg1=';
+
   this.query = '';
-  //idk what this.query should be
-  this.getPets = '';
-  this.searchURL = this.baseURL + this.apikey + '&' + this.query ;
+
+  this.searchURL = this.baseURL;
 
 
 this.getPets = function(){
   $http({
     method:'GET',
-    url: this.searchURL + this.getPets
+    url: this.searchURL + this.query
   }).then((response) => {
-    this.pets = response.data.Search;
+    this.pets = response.data;
   }, (error) =>{
     console.error( error );
   }).catch (error => {
