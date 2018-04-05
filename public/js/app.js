c42caa570cd00ce1f4703ecdb5849e4930165ebc
Allyson Finck <allyson.finck@gmail.com>
2018-04-05 11:14:00 -0400

    edit url

--- public/js/app.js
+++ public/js/app.js
@@ -14,14 +14,14 @@ app.controller('MainController', ['$http', function($http){
   this.searchURL = this.baseURL;
 
 
-this.getPets = function(){
+this.getPets = ['$sce', function($sce){
   const url = 'https://api.petfinder.com/pet.find?key=9ea203268b6ec8547b6ba71eb1241987&location=10940&format=json';
   const trustedUrl = $sce.trustAsResourceUrl(url);
 // const url = this.searchURL + this.query + '&format=json';
   $http.jsonp(trustedUrl, {jsonPCallbackPAram: 'callback'})
     .then(function(data){
       console.log(data.found);
-    })
+    })]
   // $http({
   //   method:'GET',
   //   url: url
