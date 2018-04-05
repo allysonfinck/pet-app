7f008a72626cf3e5a74bf1e8130ddd201716c9e6
Allyson Finck <allyson.finck@gmail.com>
2018-04-05 19:01:45 -0400

    show pets functional!

--- public/js/app.js
+++ public/js/app.js
@@ -9,109 +9,95 @@ app.controller('MainController', ['$http', '$sce', function($http, $sce){
 
   this.baseURL = 'http://api.petfinder.com/pet.find?key=d9f0af5e7a062cb5c0fec14bb266231b&location=10940';
 
-  this.query = '';
 
-  this.searchURL = this.baseURL;
+  this.getPets = function(){
+    const url = 'https://api.petfinder.com/pet.find?key=9ea203268b6ec8547b6ba71eb1241987&location=10940&format=json';
+    const trustedUrl = $sce.trustAsResourceUrl(url);
+
+    $http.jsonp(trustedUrl, {jsonpCallbackParam: 'callback'})
+      .then(function(response){
+        console.log(response.data.petfinder.pets.pet);
+        controller.pets = response.data.petfinder.pets.pet;
+      })
+    }
+
+  // *****testing register/login******
+    this.createUser = function(){
+      $http({
+        method:'POST',
+        url: '/users',
+        data: {
+          username: this.username,
+          password: this.password
+        }
+      }).then(function(response){
+        console.log(response);
+      }, function(){
+        console.log('error');
+      });
+    }
+
+    this.logIn = function(){
+      $http({
+        method: 'POST',
+        url:'/sessions',
+        data: {
+          username: this.loginUsername,
+          password: this.loginPassword
+        }
+      }).then(function(response){
+        controller.loggedinUsername = response.data.loginUsername;
+        console.log(response);
+      }, function(){
+        console.log('error');
+      })
+    }
+
+    this.logOut = function(){
+      $http({
+        method: 'DELETE',
+        url: '/sessions'
+      }).then(function(response){
+        console.log(response);
+      }, function(){
+        console.log('error');
+      })
+    }
+
+    this.getPets();
+
+  }]) //closes app.controller
 
 
-this.getPets = function(){
-  const url = 'https://api.petfinder.com/pet.find?key=9ea203268b6ec8547b6ba71eb1241987&location=10940&format=json';
-  const trustedUrl = $sce.trustAsResourceUrl(url);
+  //******modal*******
 
-  $http.jsonp(trustedUrl, {jsonpCallbackParam: 'callback'})
-    .then(function(data){
-      console.log(data.found);
-    })
-  // $http({
-  //   method:'GET',
-  //   url: url
-  // }).then((response) => {
-  //   console.log(response.data);
-  //   // this.pets = response.data;
-  // }, (error) =>{
-  //   console.error( error );
-  // }).catch (error => {
-  //   console.error('Catch:', error)
-  // })
-}
+  $( ()=>{
 
-// *****testing register/login******
-  this.createUser = function(){
-    $http({
-      method:'POST',
-      url: '/users',
-      data: {
-        username: this.username,
-        password: this.password
-      }
-    }).then(function(response){
-      console.log(response);
-    }, function(){
-      console.log('error');
-    });
+  //grabbing the login/register button
+  const $openBtn = $('#openModal');
+
+  //grabbing modal elememnt
+  const $modal = $('#modal');
+
+  //grabbing close button
+  const $closeBtn = $('#close');
+
+
+  //event handlers
+  const openModal = ()=>{
+    $modal.css('display', 'block');
   }
 
-  this.logIn = function(){
-    $http({
-      method: 'POST',
-      url:'/sessions',
-      data: {
-        username: this.loginUsername,
-        password: this.loginPassword
-      }
-    }).then(function(response){
-      controller.loggedinUsername = response.data.loginUsername;
-      console.log(response);
-    }, function(){
-      console.log('error');
-    })
+  const closeModal = ()=>{
+    // console.log('close modal test');
+    $modal.css('display', 'none');
   }
 
-  this.logOut = function(){
-    $http({
-      method: 'DELETE',
-      url: '/sessions'
-    }).then(function(response){
-      console.log(response);
-    }, function(){
-      console.log('error');
-    })
-  }
-
-  this.getPets();
-
-}]) //closes app.controller
 
 
-//******modal*******
+  //event listeners
+  $openBtn.on('click', openModal);
 
-$( ()=>{
-
-//grabbing the login/register button
-const $openBtn = $('#openModal');
-
-//grabbing modal elememnt
-const $modal = $('#modal');
-
-//grabbing close button
-const $closeBtn = $('#close');
-
-
-//event handlers
-const openModal = ()=>{
-  $modal.css('display', 'block');
-}
-
-const closeModal = ()=>{
-  // console.log('close modal test');
-  $modal.css('display', 'none');
-}
-
-
-
-//event listeners
-$openBtn.on('click', openModal);
-
-$closeBtn.on('click', closeModal);
+  $closeBtn.on('click', closeModal);
 
 }); //
